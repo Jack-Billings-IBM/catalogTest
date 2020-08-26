79c401a5fd4d1124678591a05a33be8d0c105153
Jack-Billings-IBM <44097415+Jack-Billings-IBM@users.noreply.github.com>
2020-08-26 11:39:34 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,16 +5,16 @@ node('master') {
    def zceeHome
    stage('Checkout Git Code') { // for display purposes
       // Get some code from a GitHub repository
-      //println "project name is catalogTest"
-      //dir ("catalogTest") {
-        //git credentialsId: 'git', url: 'https://github.com/Jack-Billings-IBM/catalogTest.git'
+      println "project name is catalogTest"
+      dir ("catalogTest") {
+        git credentialsId: 'git', url: 'https://github.com/Jack-Billings-IBM/catalogTest.git'
 
          // Get the Maven tool.
         // ** NOTE: This 'M3' Maven tool must be configured
         // **       in the global configuration.           
         //mvnHome = tool 'M3'
         
-        //}
+        }
    }
     stage('Compile zOS Connect Source Project') {
         println "Calling zconbt"

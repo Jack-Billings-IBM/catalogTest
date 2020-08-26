db084cef67f37631cf5abe95b98c1845afcf33cb
Jack-Billings-IBM <44097415+Jack-Billings-IBM@users.noreply.github.com>
2020-08-26 17:02:03 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -142,7 +142,10 @@ node('master') {
       def respCode = ""
       
       //def single = readJSON file: 'tests/inquireSingle_service_request.json'
-      single = '{\"DFH0XCMNOperation":{"ca_request_id":"01INQS","ca_inquire_single":{"ca_item_ref_req":20}}}'
+      //single = '{\"DFH0XCMNOperation":{"ca_request_id":"01INQS","ca_inquire_single":{"ca_item_ref_req":20}}}'
+      {"deployment": {"revision": "v1","user": "me"}}'
+      def json = JsonOutput.toJson([ deployment : [ revision: "v1", user: "me" ] ])`
+      def single = JsonOutput.toJson([ DFH0XCMNOperation : [ ca_request_id: "01INQS", [ ca_inquire_single:  ]]])`
       catalog = '{"DFH0XCMNOperation":{"ca_request_id":"01INQC","ca_inquire_request":{"ca_list_start_ref":20}}}'
       
       def command_val = 'curl -X POST -o ${WORKSPACE}/tests/'+serviceName+'_service.json -w %{response_code} --header "Content-Type: application/json" --header "Content-Type: plain/text" --data '+single+' --insecure '+urlval

311dd20209f1a500bf00d8f7a4ba9e02886c9fa3
Jack-Billings-IBM <44097415+Jack-Billings-IBM@users.noreply.github.com>
2020-08-26 15:02:18 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -132,7 +132,7 @@ node('master') {
       single = '{"DFH0XCMNOperation":{"ca_request_id":"01INQS","ca_inquire_single":{"ca_item_ref_req":20}}}'
       catalog = '{"DFH0XCMNOperation":{"ca_request_id":"01INQC","ca_inquire_request":{"ca_list_start_ref":20}}}'
       
-      def command_val = 'curl -X POST -o ${WORKSPACE}/catalogTest/test/'+serviceName+'_service.json -w %{response_code} --header "Content-Type: application/json" --header "Content-Type: text/plain" --data tests/'+serviceName+'_service_request.json --insecure '+urlval
+      def command_val = 'curl -X POST -o ${WORKSPACE}/catalogTest/tests/'+serviceName+'_service.json -w %{response_code} --header "Content-Type: application/json" --header "Content-Type: text/plain" --data tests/'+serviceName+'_service_request.json --insecure '+urlval
       respCode = sh (script: command_val, returnStdout: true)
       //def command_val = "curl -X POST -o response.json -w %{response_code} --header 'Authorization:Basic $usercred' --header 'Content-Type:application/zip' --data-binary @/sarfiles/"+sarFileName+" --insecure "+urlval
       println "Service Installation Response code is: "+respCode

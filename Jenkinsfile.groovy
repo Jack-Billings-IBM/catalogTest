7e028b488857a73d33e94d1c88b18b0d8b19e6fe
Jack-Billings-IBM <44097415+Jack-Billings-IBM@users.noreply.github.com>
2020-08-26 11:59:08 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -30,6 +30,9 @@ node('master') {
 
 node('zOS') {
    stage('Update Copybooks on zOS') {
+      dir ("catalogTest") {
+        git credentialsId: 'git', url: 'https://github.com/Jack-Billings-IBM/catalogTest.git'
+      }
       env.JAVA_HOME = "/usr/lpp/java/J8.0_64"
       env.PATH="${env.JAVA_HOME}/bin:${env.PATH}"
       echo "java -v"

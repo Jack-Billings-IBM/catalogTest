0b4ba1be996db535c6c13e1b0286f01776ec2237
Jack-Billings-IBM <44097415+Jack-Billings-IBM@users.noreply.github.com>
2020-08-27 11:04:30 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -153,7 +153,8 @@ node('master') {
       def catalog = '{"DFH0XCMNOperation":{"ca_request_id":"01INQC","ca_inquire_request":{"ca_list_start_ref":20}}}'
       
       //def command_val = 'curl -X POST -o ${WORKSPACE}/tests/'+serviceName+'_service.json -w %{response_code} --header "Content-Type: application/json" --header "Content-Type: plain/text" --data '+single+' --insecure '+urlval
-      def command_val = 'curl --location --request POST '+urlval+' --header "Content-Type: application/json" --header "Content-Type: text/plain" --data '+single+''
+      //def command_val = 'curl --location --request POST '+urlval+' --header "Content-Type: application/json" --header "Content-Type: text/plain" --data '+single+''
+      def command_val 'curl --location --fail --request POST "http://10.1.1.2:9080/zosConnect/services/inquireCatalog?action=invoke" --header "Content-Type: application/json" --header "Content-Type: text/plain" --data '+catalog+' -o tests/inquireCatalog_service.json'
       respCode = sh (script: command_val, returnStdout: true)
       //def command_val = "curl -X POST -o response.json -w %{response_code} --header 'Authorization:Basic $usercred' --header 'Content-Type:application/zip' --data-binary @/sarfiles/"+sarFileName+" --insecure "+urlval
       println "Service Installation Response code is: "+respCode

81aeff1bfaebf3e266b11629fea51365026a83a6
Jack-Billings-IBM <44097415+Jack-Billings-IBM@users.noreply.github.com>
2020-08-27 15:36:45 -0400

    Update Jenkinsfile_api.groovy

--- Jenkinsfile_api.groovy
+++ Jenkinsfile_api.groovy
@@ -18,7 +18,7 @@ node('master') {
         println "Calling zconbt"
         def output = sh (returnStdout: true, script: 'pwd')
         println output
-        sh "${WORKSPACE}/zconbt/bin/zconbt --projectDirectory=${WORKSPACE}/catalog --file=${WORKSPACE}/archives/catalog.aar "
+        sh "${WORKSPACE}/zconbt/bin/zconbt -pd=${WORKSPACE}/catalog -f=${WORKSPACE}/archives/catalog.aar "
         println "Called zconbt for catalog"
    }
    stage('Check for and Handle Existing Service') {

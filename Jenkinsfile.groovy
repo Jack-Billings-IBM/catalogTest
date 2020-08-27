40e0e4c485a57448292bbeb5654ad22bfc6cd3b5
Jack-Billings-IBM <44097415+Jack-Billings-IBM@users.noreply.github.com>
2020-08-27 11:42:47 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -151,7 +151,7 @@ node('master') {
       //def single = readJSON file: 'tests/inquireSingle_service_request.json'
       def single = '''{"DFH0XCMNOperation":{"ca_request_id":"01INQS","ca_inquire_single":{"ca_item_ref_req":40}}}'''
 
-      def catalog = ''{"DFH0XCMNOperation":{"ca_request_id":"01INQC","ca_inquire_request":{"ca_list_start_ref":20}}}''
+      def catalog = '''{"DFH0XCMNOperation":{"ca_request_id":"01INQC","ca_inquire_request":{"ca_list_start_ref":20}}}'''
       
       //def command_val = 'curl -X POST -o ${WORKSPACE}/tests/'+serviceName+'_service.json -w %{response_code} --header "Content-Type: application/json" --header "Content-Type: plain/text" --data '+single+' --insecure '+urlval
       def command_val = 'curl --location --fail --request POST '+urlval+' --header "Content-Type: application/json" --header "Content-Type: text/plain" --data '+single+''

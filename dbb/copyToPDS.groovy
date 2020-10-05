d3455e9c71ccb100d671dbf905ba4ca61b940419
Jack-Billings-IBM <44097415+Jack-Billings-IBM@users.noreply.github.com>
2020-10-05 15:54:47 -0500

    Update copyToPDS.groovy

--- dbb/copyToPDS.groovy
+++ dbb/copyToPDS.groovy
@@ -1,4 +1,4 @@
 import com.ibm.dbb.build.*
 
 // Copy a entire directory to PDS
-  new CopyToPDS().file(new File("dbb/catalog/copybook")).dataset("DFH550.CICS.SDFHSAMP").execute()
+  new CopyToPDS().file(new File("dbb/catalog/copybook")).dataset("JBILL.CICS.SDFHSAMP").execute()
